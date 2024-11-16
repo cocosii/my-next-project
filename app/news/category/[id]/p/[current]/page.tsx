8d3afb34e3906f18477aad625e89bc9f08ee37c3
cocosii <cocosii.choco@gmail.com>
2024-11-16 17:12:44 +0900

    6

--- app/news/category/[id]/p/[current]/page.tsx
+++ app/news/category/[id]/p/[current]/page.tsx
@@ -1,18 +1,19 @@
-import { notFound } from "next/navigation";
-import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
-import NewsList from "@/app/_components/NewsList";
-import Pagination from "@/app/_components/Pagination";
-import { NEWS_LIST_LIMIT } from "@/app/_constants";
+import { notFound } from 'next/navigation';
+import { getCategoryDetail, getNewsList } from '@/app/_libs/microcms';
+import NewsList from '@/app/_components/NewsList';
+import Pagination from '@/app/_components/Pagination';
+import { NEWS_LIST_LIMIT } from '@/app/_constants';
 
 type Props = {
   params: {
-    id: string;
     current: string;
+    id: string;
   };
 };
 
 export default async function Page({ params }: Props) {
-  const current = parseInt(params.current, 10);
+  const current = parseInt(params.current as string, 10);
+
   if (Number.isNaN(current) || current < 1) {
     notFound();
   }
--- app/news/p/[current]/page.tsx
+++ app/news/p/[current]/page.tsx
@@ -1,8 +1,8 @@
-import { notFound } from "next/navigation";
-import { getNewsList } from "@/app/_libs/microcms";
-import NewsList from "@/app/_components/NewsList";
-import Pagination from "@/app/_components/Pagination";
-import { NEWS_LIST_LIMIT } from "@/app/_constants";
+import { notFound } from 'next/navigation';
+import { getNewsList } from '@/app/_libs/microcms';
+import NewsList from '@/app/_components/NewsList';
+import Pagination from '@/app/_components/Pagination';
+import { NEWS_LIST_LIMIT } from '@/app/_constants';
 
 type Props = {
   params: {
@@ -11,10 +11,12 @@ type Props = {
 };
 
 export default async function Page({ params }: Props) {
-  const current = parseInt(params.current, 10);
+  const current = parseInt(params.current as string, 10);
+
   if (Number.isNaN(current) || current < 1) {
     notFound();
   }
+
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
     offset: NEWS_LIST_LIMIT * (current - 1),
